8259e859e7adc4b39165c8a2bb9376aa0340fa64
Marcin <marcin.traczyk.88@gmail.com>
2014-02-03 17:52:48 +0000

    ADD azzopardi case mash

---
+++ examples/azzopardi.C
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <math.h>
+#include "meshing.H"
+
+using namespace meshing;
+
+int main(){
+
+    //pipe radius
+    double R = 0.063;
+    //pipe length
+    double L = 4.5;
+    //particle injection ring inner radius
+    double injectionR = 0.9 * 0.063;
+
+    //number of cells in z direction
+    int nZ = 100;
+    //in theta direction
+    int nTheta = 36;
+    //in r direction for central seciton
+    int nR1 = 12;
+    //in r direction for ring seciton
+    int nR2 = 5;
+
+    pipe::oType centralSection
+        (
+            L,
+            injectionR,
+            0.0,
+            0.4,
+            0.44
+        );
+
+    pipe::ring ringSection
+        (
+            L,
+            injectionR,
+            R,
+            0.0,
+            4,
+            M_PI / 4.0
+        );
+
+    centralSection.nCell(nR1, nTheta, nZ);
+    ringSection.nCell(nR2, nTheta , nZ);
+
+    mesh M;
+    M.addElement(centralSection);
+    M.addElement(ringSection);
+
+    M.addPatch(r, R, "walls", w);
+    M.addPatch(z, L, "outlet", p);
+    M.addPatch(z, 0.0, "oilInlet", p);
+    M.separateFromPatch("oilInlet",r, injectionR, R, "waterInlet", p);
+
+    M.write();
+}
